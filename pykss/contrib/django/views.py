a5d2c1a96e810dd2f0316269929a7bc8395add53
Sean Brant <brant.sean@gmail.com>
2013-03-10 22:58:56 -0500

    Adds django view tests

--- pykss/contrib/django/views.py
+++ pykss/contrib/django/views.py
@@ -1,17 +1,17 @@
 from django.conf import settings
-from django.views.generic.base import TemplateView
+from django.views.generic.base import ContextMixin, TemplateView
 
 import pykss
 
 
-class StyleGuideMixin(object):
+class StyleGuideMixin(ContextMixin):
 
-    def get_styleguid(self):
+    def get_styleguide(self):
         dirs = getattr(settings, 'PYKSS_DIRS', [])
         return pykss.Parser(*dirs)
 
     def get_context_data(self, **kwargs):
-        context = {'styleguide': self.get_styleguid()}
+        context = {'styleguide': self.get_styleguide()}
         context.update(kwargs)
         return super(StyleGuideMixin, self).get_context_data(**context)
 
---
+++ tests/test_contrib/test_django/test_views.py
@@ -0,0 +1,18 @@
+import os
+
+from django.conf import settings
+from django.test import TestCase
+
+from pykss.contrib.django.views import StyleGuideMixin
+
+
+class StyleGuideMixinTestCase(TestCase):
+
+    def test_get_styleguide_get_dirs_from_settings(self):
+        css = os.path.join(settings.PROJECT_ROOT, 'tests', 'fixtures', 'css')
+        with self.settings(PYKSS_DIRS=[css]):
+            styleguide = StyleGuideMixin().get_styleguide()
+            self.assertEqual(styleguide.section('2.1.1').description, 'Your standard form button.')
+
+    def test_get_context_data_adds_sytleguide_to_context(self):
+        self.assertIn('styleguide', StyleGuideMixin().get_context_data())
