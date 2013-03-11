128e67e1a07bd9292b24ae4c63f812d3c027c0d2
Sean Brant <brant.sean@gmail.com>
2013-03-11 08:23:35 -0500

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pykss',
-    version='0.2.1',
+    version='0.2.2',
     description='Python implementation of KSS',
     long_description=open('README.rst').read(),
     author='Sean Brant',
